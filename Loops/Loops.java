f770a0b5721bfa6acd5b6f043ee04c027f67c14e
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-02-03 17:58:50 +0530

    Diamond Pattern

---
+++ Loops/Loops.java
@@ -0,0 +1,28 @@
+package Loops;
+
+public class Loops {
+    public static void main(String[] args) {
+        int n= 5;
+        for(int i=0; i<n; i++){
+            for(int k=1; k<n-i;k++){
+                System.out.print(" ");
+            };
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for(int i=0; i<n; i++){
+            
+            for(int j=1; j<=i; j++){
+
+                System.out.print(" ");
+            }
+            for(int k=1; k<n-i;k++){
+                System.out.print(" *");
+            };
+            System.out.println();
+        }
+    }
+}
