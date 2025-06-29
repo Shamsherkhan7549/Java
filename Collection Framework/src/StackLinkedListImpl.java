3a2ad407361bb3e9b7938f56b81bb64dcd1298bb
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-06-29 05:28:44 -0700

    Stack Implementation using LinkedList

---
+++ Collection Framework/src/StackLinkedListImpl.java
@@ -0,0 +1,78 @@
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+
+
+class Stack <T>{
+
+    LinkedList<T> ll = new LinkedList<>();
+
+    void push(T element){
+        ll.addLast(element);
+    }
+
+    T peek(){
+        if(isEmpty()){
+            System.out.println("Stack is empty!");
+            throw new NoSuchElementException("Stack is empty!");
+        }
+
+        return ll.getLast();
+    }
+
+    T pop(){
+        if(isEmpty()){
+            System.out.println("Stack is empty!");
+            throw new NoSuchElementException("Stack is empty!");
+        }
+
+        return ll.removeLast();
+    }
+
+    void print(){
+        if(isEmpty()){
+            System.out.println("Stack is empty!");
+            throw new NoSuchElementException("Stack is empty!");
+        }
+
+        for(int i = 0; i < ll.size(); i++){
+            System.out.print(ll.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    boolean isEmpty(){
+        if(ll.size() == 0){
+            return true;
+        }
+        return false;
+    }
+
+    int size(){
+        return ll.size();
+    }
+   
+}
+
+public class StackLinkedListImpl {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(3);
+        stack.push(4);
+        stack.push(6);
+        stack.push(7);
+        stack.print();
+        System.out.println("Popped element : " + stack.pop());
+        System.out.println("Peeked element : " + stack.peek());
+        stack.print();
+        System.out.println("Size : " + stack.size());
+        System.out.println("isEmpty : " + stack.isEmpty());
+
+         Stack<String> stack2 = new Stack<>();
+        stack2.push("shamsher");
+        stack2.push("rehan");
+        stack2.push("samar");
+        stack2.print();
+        System.out.println("size stack2 : " + stack2.size());
+
+    }
+}
