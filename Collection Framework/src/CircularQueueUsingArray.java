6af28370c4c2e3d03f81e1b3ba0a560bd22e866c
Shamsher Khan <shamsherkhan7549@gmail.com>
2025-07-12 00:31:11 -0700

    CircularQueue Using Array

--- Collection Framework/src/CircularQueueUsingArray.java
+++ Collection Framework/src/CircularQueueUsingArray.java
@@ -7,28 +7,50 @@ class Queue<T>{
     Object arr[] = new Object[length];
 
     void push(T element){
-        if(rear == length && size < length){
-            rear = 0;
+
+        if(rear == length && size<length){
+            arr[0] = element;
+            rear = 1;
+            size++;
+        }else if(size<length){
             arr[rear] = element;
             rear++;
             size++;
         }else{
-            arr[rear] = element;
-            rear++;
-            size++;
-        }
+            System.out.println("Queue is full");
+        }   
+        
     }
 
     T remove(){
+       if(front == length && size > 0){
+            T f = (T) arr[0];
+            arr[0] = 0;
+            size--;
+            front = 1;
+            return f;
+       }else if(size>0){
         T f =  (T) arr[front];
         arr[front] = 0;
         front++;
         size--;
         return f;
+       }
+       
+       System.out.println("Array is empty");
+       return null;
     }
 
-    T peek(){
+    T peek() throws Exception{
+        if(size==0){
+            throw new Exception("Queue is empty");
+        }
         return (T) arr[front];
+    }
+
+    boolean isFull(){
+         if(size == length) return true;
+        return false;
     }
 
     boolean isEmpty(){
@@ -37,10 +59,20 @@ class Queue<T>{
     }
 
     void print(){
-        for(int i = front; i < rear; i++){
-            System.out.print(arr[i] + " ");
-        }
+           if(front>=rear){
+             for(int i = front; i < length ; i++){
+                System.out.print(arr[i] + " ");
+            }
+            for(int i = 0; i < rear ; i++){
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
+           }
 
+            for(int j = front; j < rear; j++ ){
+               System.out.print(arr[j] + " ");
+            }
         System.out.println();
     }
 
@@ -54,16 +86,20 @@ public class CircularQueueUsingArray {
         queue.push(8);
         queue.push(9);
         queue.push(2);
+        // System.out.println("peeked element: " + queue.peek());
+        // System.out.println("removed element: " + queue.remove());
+        // System.out.println( "rear " + queue.rear);
+        // System.out.println( "front " + queue.front);
+        // System.out.println("peeked element: " + queue.peek());
         queue.print();
-        System.out.println("peeked element: " + queue.peek());
-        System.out.println("removed element: " + queue.remove());
-        queue.remove();       
         queue.remove();
-        System.out.println( "rear " + queue.rear);
-
-        // queue.push(5);       
+        queue.remove();
+        
+        queue.push(1);
+        queue.push(2);
         queue.print();
-        System.out.println("peeked element: " + queue.peek());
+
+
 
 
 
